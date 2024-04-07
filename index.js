adc8d8cb4c8cae5b89239ae79533a8360aa8b296
unknown <ahsnova557@gmail.com>
2024-04-07 09:24:10 +0400

    task added

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ console.log("Kreditin aylıq hissələrə bölünmüş ödənişləri:");
 var hisseler = kreditHisselereBol(kreditMebliyi, aySayi);
 for (let j = 0; j < hisseler.length; j++) {
     console.log((j + 1) + "-ci ay: " + hisseler[j] + " manat");
-// }
+}
+
+// task3
 function fahrenheitToCelsius(fahrenheit) {
    
     let celsius = (fahrenheit - 32) * 5 / 9;
@@ -37,3 +39,22 @@ let fahrenheitDerece = 75;
 
 
 console.log(fahrenheitDerece + " Fahrenheit derecesi " + fahrenheitToCelsius(fahrenheitDerece) + " Celsius derecesine bearberdir.");
+
+// task2
+let array = [1,2,3,4,4]
+function num (array){
+    let eded =0
+    for (let i=0; i< array.length ;i++){
+        for (let j =1; j < array.length ;j++) {
+            
+            if (array[j]==[i]) {
+                eded=array[i]
+                
+            }
+        }
+        
+    }
+    return eded
+}
+console.log(num(array))
+    
